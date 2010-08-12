9c7302d34c7664fd275d36d18d27b05c71cd9f6e
Bjørnar Grip Fjær <bjornar.fjar@gmail.com>
2010-08-12 11:47:54 +0200

    Added GPL headers to files.

--- client/testers/rsync.py
+++ client/testers/rsync.py
@@ -1,3 +1,20 @@
+# -*- coding: utf-8 -*-
+#
+#            testers/rsync.py is part of SPODTest.
+#
+# All of SPODTest is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# SPODTest is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with SPODTest.  If not, see <http://www.gnu.org/licenses/>.
+
 from testers.base import CommandBuilder, Args
 
 class RSyncCommand(CommandBuilder):
